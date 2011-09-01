8b4d68a35f5f1f239b3a554fbe3db541f69e9355
Evgeny Reznichenko <kusakyky@gmail.com>
2011-09-02 01:07:11 +0400

    Add test class.

--- index.js
+++ index.js
@@ -35,7 +35,64 @@ var c = connect.createServer(
 				res.setHeader('Content-type', 'text/javascript');
 				res.end('(function(w,s){s.return='+ str +';})(window, PANZERWEB);');				
 			});
+			
+			app.get('/api/point/get/:fname', function (req, res) {
+				var str = JSON.stringify(test.point).replace(/^"|"$/g, '');
+				res.setHeader('Content-type', 'text/javascript');
+				res.end('(function(w,s){s.return='+ str +';})(window, PANZERWEB);');
+			});
 	})
 );
+
+function Test() {
+	this.width  = 800;
+	this.height = 600;
+	this.point  = {x: 0, y: 0, dx: -1, dy: -2};
+	this._lastTime = Date.now();
+	this._interval = 100;
+		
+	this.run();
+};
+
+Test.prototype.run = function () {
+	var self = this;
+	
+	function tack() {
+		var tickTime = Date.now()
+			, dt = tickTime - self._lastTime
+			, nt
+			;
+		
+		if (dt >= self._interval) {
+			self.tick(dt);;
+			self._lastTime = Date.now();
+			nt = self._interval - (self._lastTime - tickTime);
+		} else {
+			nt = self._interval - dt;
+		}
+		
+		nt = (nt < 0)? 0: nt;
+		setTimeout(tack, nt);
+	};
+	
+	tack();
+};
+
+Test.prototype.tick = function (dt) {
+	var p = this.point;
+	
+	p.x += p.dt;
+	p.y += p.dy;
+	
+	if (p.x > this.width) {
+		p.x = 0;
+	}
+	
+	if (p.y > this.height) {
+		p.y = 0;
+	}
+};
+
+var test = new Test();
 
 c.listen(7654);
